7902cb821d55ba8955838d293788e83ff3b07f80
DrDoXi <lhamdouchidrisss2@gmail.com>
2022-10-01 20:14:47 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -82,7 +82,7 @@ map.on('load', () => {
         "paint": {
             "circle-radius": 10,
             "circle-color": "#5b94c6",
-            "circle-opacity": 0.5
+            "circle-opacity": 0
         }   
     });
 
