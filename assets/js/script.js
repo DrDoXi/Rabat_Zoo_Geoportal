07c068a991895fc67c66f3f57ad5a6fa6d1f708e
Lhamdouchi Driss <lhamdouchidrisss2@gmail.com>
2023-02-15 13:45:44 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -701,10 +701,10 @@ map.on('load', () => {
 			'<nav class="nav container"><div class="nav__menu" id="nav-menu"><ul class="nav__list">' +
 			'<li id="open-sheett" class="nav__item"><a href="#home" class="nav__link "><i class=' +
 			"'bx bx-info-circle nav__icon'" +
-			'></i><span class="nav__name">Details</span></a></li>' +
+			'></i><span class="nav__name">Détails</span></a></li>' +
 			'<li id="Direction_btnn" class="nav__item"><a href="#about" class="nav__link"><i class=' +
 			"'bx bx-navigation nav__icon'" +
-			'></i><span class="nav__name">Drirection</span></a></li>' +
+			'></i><span class="nav__name">Itinéraire</span></a></li>' +
 			'</ul></div></nav>';
 
 		// Ensure that if the map is zoomed out such that multiple
